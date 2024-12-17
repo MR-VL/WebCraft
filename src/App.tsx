ecb2cf9a15fb20f92eb068289277b1a86aaaf66e
MR-VL <107425742+MR-VL@users.noreply.github.com>
2024-12-17 16:34:40 -0600

    start threejs-component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
 import './App.css'
 import * as three from 'three'
-
+import {EntityManager} from './EntityManager.tsx'
+import {Entity} from "./Entity.tsx";
+import {threejs_component} from './threejs-component.js';
 class WebCraft{
+    private entityManager: EntityManager;
 
 
     constructor() {
         this.Initialize();
     }
 
-    private Initialize(){
+    public Initialize(){
+         this.entityManager = new EntityManager.EntityManager();
 
     }
+
+    public LoadControllers(){
+        const threejs = new Entity.Entity();
+        threejs.AddComponent(new threejs_component.ThreeJSController() )
+    }
+
 }
