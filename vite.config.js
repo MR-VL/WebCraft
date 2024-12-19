038d903f848eedb981c813d65d81f5b1251fecd2
MR-VL <107425742+MR-VL@users.noreply.github.com>
2024-12-18 19:35:22 -0600

    initial project setup due to js refactor

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import wasm from 'vite-plugin-wasm';
+import topLevelAwait from 'vite-plugin-top-level-await';
+import solidPlugin from 'vite-plugin-solid';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [wasm(), topLevelAwait(), solidPlugin()],
+  resolve: {
+    alias: {
+    },
+  },
+  build: {
+    sourcemap: true,
+  },
+  server: {
+    port: 5200,
+    hmr: {
+      clientPort: 5200,
+    }
+  },
+  base: "/WebCraft/"
+});
