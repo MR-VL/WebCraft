eca8b5bfd182da8d02b84a87c06745c3253f5cf5
MR-VL <107425742+MR-VL@users.noreply.github.com>
2024-12-21 19:54:27 -0600

    finish component reqs

--- src/Entity.js
+++ src/Entity.js
@@ -22,13 +22,84 @@ export const entity = (() => {
             this.name = name;
         }
 
+        get Name(){
+            return this.name;
+        }
+
         InitEntity(){
             for(let k in this.components){
                 this.components[k].InitEntity();
             }
         }
 
+        Destroy(){
+            for(let k in this.components){
+                this.components[k].Destroy();
+            }
+            this.components = null;
+            this.parent = null;
+            this.handlers = null;
+        }
+
+        SetActive(bool){
+            this.parent.SetActive(this, bool);
+        }
+
+        SetDead(){
+            this.dead = true;
+        }
+
     }
 
+    class Component{
+        get name(){
+            console.error('Unnamed Component: ' + this.constructor.name);
+            return '__UNNAMED__';
+        }
 
-})
+        constructor() {
+            this.parent = null;
+        }
+
+        Destroy(){}
+
+        SetParent(parent){
+            this.parent = parent;
+        }
+
+        InitComponent(){}
+
+        InitEntity(){}
+
+        GetComponent(name){
+            return this.parent.GetComponent(name);
+        }
+
+        get Manager(){
+            return this.parent.Manager;
+        }
+
+        get Parent(){
+            return this.parent;
+        }
+
+        FindEntity(name){
+            return this.parent.FindEntity(name);
+        }
+
+        BroadCast(message){
+             this.parent.Broadcast(message);
+        }
+
+        Update(_){}
+
+        RegisterHandler(name, handler){
+            this.parent.RegisterHandler(name, handler);
+        }
+    }
+    return{
+        Entity: Entity,
+        Component: Component
+    }
+
+})();
