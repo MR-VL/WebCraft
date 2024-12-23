496dfc51afd4f32f5ef6706dcae7e9a1876fb214
MR-VL <107425742+MR-VL@users.noreply.github.com>
2024-12-22 19:34:52 -0600

    populate defs, still need colors

--- src/defs.js
+++ src/defs.js
@@ -4,7 +4,12 @@ export const defs = (() =>{
 
     return{
         FogRange: [100, 300],
-
-
+        UnderWaterRange: [0,50],
+        FogColor: new THREE.Color(0xcdd8f1).convertSRGBToLinear(),
+        MoonColor: new THREE.Color(0x).convertSRGBToLinear(),
+        UnderWaterColor: new THREE.Color(0x).convertSRGBToLinear(),
+        SkyColor: new THREE.Color(0x).convertSRGBToLinear(),
+        PlayerPOS: [255.311252087425, 100, 290.98564212457086],
+        PlayerROT:[0.02753162419089479, -0.7573631733845853, 0.031998988835540886, 0.6516280365237096]
     }
 })();
