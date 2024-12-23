1c02bfd7dabff611e7b3e5012b76a176189b5721
MR-VL <107425742+MR-VL@users.noreply.github.com>
2024-12-23 14:23:08 -0600

    populate colors

--- src/defs.js
+++ src/defs.js
@@ -6,9 +6,9 @@ export const defs = (() =>{
         FogRange: [100, 300],
         UnderWaterRange: [0,50],
         FogColor: new THREE.Color(0xcdd8f1).convertSRGBToLinear(),
-        MoonColor: new THREE.Color(0x).convertSRGBToLinear(),
-        UnderWaterColor: new THREE.Color(0x).convertSRGBToLinear(),
-        SkyColor: new THREE.Color(0x).convertSRGBToLinear(),
+        MoonColor: new THREE.Color(0xf4ffbe).convertSRGBToLinear(),
+        UnderWaterColor: new THREE.Color(0x345aa2).convertSRGBToLinear(),
+        SkyColor: new THREE.Color(0x93bfdd).convertSRGBToLinear(),
         PlayerPOS: [255.311252087425, 100, 290.98564212457086],
         PlayerROT:[0.02753162419089479, -0.7573631733845853, 0.031998988835540886, 0.6516280365237096]
     }
