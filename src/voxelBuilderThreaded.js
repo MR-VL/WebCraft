4f369810c8970212991e47565dcccb8a106f59c6
MR-VL <cosmmo6@gmail.com>
2025-01-10 15:30:32 -0600

    setters + getters

--- src/voxelBuilderThreaded.js
+++ src/voxelBuilderThreaded.js
@@ -5,6 +5,66 @@ import {GameDefs} from "./Game-defs.js";
 
 export const voxelBuilderThreaded = (() => {
 
+    class SparseVoxelCellBlock{
+        constructor(params){
+            this.params = params;
+            this.voxles = {};
+            this.group = new THREE.Group();
+            this.buildID = 0;
+            this.lastBuildID = -1;
+            this.building = false;
+            this.dirty = false;
+            this.builder = new voxelBlockBuilder.VoxelBlockBuilder();
+            params.scene.add(this.group);
+        }
+
+        Destroy(){
+            this.ReleaseAssets();
+            this.group.parent.remove(this.group);
+        }
+
+        ReleaseAssets(){
+            this.group.traverse(current => {
+                if(current.material){
+                    current.material.dispose();
+                }
+                if(current.geometry){
+                    current.geometry.dispose();
+                }
+            });
+            if (this.opaqueMesh){
+                this.group.remove(this.opaqueMesh);
+            }
+
+            if(this.transparentMesh){
+                this.group.remove(this.transparentMesh);
+            }
+        }
+
+        Show(){
+            this.group.visible = true;
+        }
+
+        Hide(){
+            this.group.visible = false;
+        }
+
+        get Destroyed(){
+            return !this.group.parent;
+        }
+
+        get Dirty(){
+            return this.dirty
+        }
+
+        Key(x, y, z){
+            return x + '.' + y + '.' + z;
+        }
+
+
+
+    }
+
 
     return{
         VoxelBuilderThreaded : VoxelBuilderThreaded
