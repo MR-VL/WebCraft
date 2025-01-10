551a179f9be9a760dfb98410c56876f8c1813720
MR-VL <cosmmo6@gmail.com>
2025-01-10 15:46:42 -0600

    insert voxel

--- src/voxelBuilderThreaded.js
+++ src/voxelBuilderThreaded.js
@@ -8,7 +8,7 @@ export const voxelBuilderThreaded = (() => {
     class SparseVoxelCellBlock{
         constructor(params){
             this.params = params;
-            this.voxles = {};
+            this.voxels = {};
             this.group = new THREE.Group();
             this.buildID = 0;
             this.lastBuildID = -1;
@@ -59,6 +59,40 @@ export const voxelBuilderThreaded = (() => {
 
         Key(x, y, z){
             return x + '.' + y + '.' + z;
+        }
+
+        InsertVoxelAt(position, type, skippable){
+            const key = this.Key(...position);
+            if(key in this.voxels && skippable){
+                return;
+            }
+
+            //todo code cleanup
+            const voxel = {
+                position: [...position],
+                type: type,
+                visible: true
+            };
+
+            this.voxels[key] = voxel;
+            this.buildID++;
+            this.dirty = true;
+
+            const neighbors = this.params.parent.GetAdjacentBlocks(this.params.offset.x, this.params.offset.z);
+
+            for(let xi = -1; xi <= 1; ++xi){
+                for(let yi = -1; yi <= 1; ++yi){
+                    for(let zi = -1; zi <= 1; ++zi){
+                        for(let ni=0; ni < neighbors.length; ++ni){
+                            const key = this.Key(position[0]+xi, position[1]+yi, position[2]+zi);
+                            if(key in neighbors[ni].voxels){
+                                neighbors[ni].buildID++;
+                                neighbors[ni].dirty = true;
+                            }
+                        }
+                    }
+                }
+            }
         }
 
 
