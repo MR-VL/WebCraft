d67eb14b839711898064b26b2acfc1287664263d
MR-VL <cosmmo6@gmail.com>
2025-01-10 13:20:17 -0600

    init voxelBuilderThreaded.js

---
+++ src/voxelBuilderThreaded.js
@@ -0,0 +1,12 @@
+import * as THREE from 'three';
+import {workerPool} from "./workerPool.js";
+import {voxelBlockBuilder} from "./voxelBlockBuilder.js";
+import {GameDefs} from "./Game-defs.js";
+
+export const voxelBuilderThreaded = (() => {
+
+
+    return{
+        VoxelBuilderThreaded : VoxelBuilderThreaded
+    };
+})();
