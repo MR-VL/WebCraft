eacf5575f3912488558001fd4ccfb817c71db1f2
MR-VL <cosmmo6@gmail.com>
2025-01-11 16:32:41 -0600

    utils module

---
+++ src/utils.js
@@ -0,0 +1,21 @@
+export const utils = (function() {
+    return{
+        DictIntersection: function(dictA, dictB) {
+            const intersection = {};
+            for(let current in dictB){
+                if(current in dictA){
+                    intersection[current] = dictA[current];
+                }
+            }
+            return intersection;
+        },
+
+        DictDifference: function(dictA, dictB) {
+            const difference = {...dictA};
+            for(let current in dictB){
+                delete difference[current];
+            }
+            return difference;
+        }
+    };
+})
