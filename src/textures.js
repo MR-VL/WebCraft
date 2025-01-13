2a0975d17f960ab0536fafed4c39fe884b05bd9d
MR-VL <cosmmo6@gmail.com>
2025-01-13 12:33:58 -0600

    getimagedata

--- src/textures.js
+++ src/textures.js
@@ -2,6 +2,29 @@ import * as THREE from "three";
 
 export const textures = (() => {
 
+    function GetImageData(image){
+        var canvas = document.createElement("canvas");
+        canvas.width = image.width;
+        canvas.height = image.height;
+
+        var context = canvas.getContext("2d");
+        context.drawImage(image, 0, 0);
+        return context.getImageData(0, 0, image.width, image.height);
+    }
+
+    class Texture {
+        constructor(){
+            this.Create();
+            this.onLoad = () => {};
+        }
+
+        Load(atlas, names){
+
+        }
+    }
 
 
+    return{
+        TextureAtlas: TextureAtlas
+    };
 })();
