f0968aef4a963e6c848e1ceb24fd2a5bd5ef3680
MR-VL <cosmmo6@gmail.com>
2025-01-13 14:17:58 -0600

    texture functions

--- src/textures.js
+++ src/textures.js
@@ -19,8 +19,56 @@ export const textures = (() => {
         }
 
         Load(atlas, names){
-
+            this.LoadAtlas(atlas, names);
         }
+
+        Create(){
+            this.manager = new THREE.LoadingManager();
+            this.loader = new THREE.TextureLoader(this.manager);
+            this.textures = {};
+
+            this.manager.onLoad = () => {
+                this.onLoad();
+            };
+        }
+
+        get Info(){
+            return this.textures;
+        }
+
+        LoadTexture(name){
+            const texture = this.loader.load(name);
+            texture.encoding = THREE.sRGBEncoding;
+            return texture;
+        }
+
+        OnLoad(){
+            for(let k in this.textures){
+                const atlas = this.textures[k];
+                const data = new Uint8Array(atlas.textures.length * 4 * 16 * 16);
+
+                for(let texture = 0; texture < atlas.textures.length; texture++) {
+                    const currentTexture = atlas.textures[texture];
+                    const currentData = GetImageData(currentTexture.image);
+                    const offset = texture * (4 * 16 * 16);
+                    data.set(currentData.data, offset);
+                }
+
+                const diffuse = new THREE.DataTexture2DArray(data, 16, 16, atlas.textures.length);
+                diffuse.format = THREE.RGBAFormat;
+                diffuse.type = THREE.UnsignedByteType;
+                diffuse.minFilter = THREE.LinearMipMapLinearFilter;
+                diffuse.magFilter = THREE.NearestFilter;
+                diffuse.wrapS = THREE.ClampToEdgeWrapping;
+                diffuse.wrapT = THREE.ClampToEdgeWrapping;
+                diffuse.generateMipmaps = true;
+                diffuse.encoding = THREE.sRGBEncoding;
+
+                atlas.atlas = diffuse;
+            }
+            this.onLoad();
+        }
+
     }
 
 
