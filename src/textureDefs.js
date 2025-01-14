aab70e0a75df31d4885737b66e437f4a4af17948
MR-VL <cosmmo6@gmail.com>
2025-01-13 19:56:56 -0600

    init defs

--- src/textureDefs.js
+++ src/textureDefs.js
@@ -1,5 +1,33 @@
 import * as THREE from 'three';
 
 export const textureDefs = (() => {
+    return{
+        DEFS:{
+            ocean:{
+                color: new THREE.Color(0xffffff),
+                texture: 'ocean.png'
+            },
+            dirt:{
+                color: new THREE.Color(0xffffff),
+                texture: 'dirt.png'
+            },
+            sand:{
+                color: new THREE.Color(0xffffff),
+                texture: 'sand.png'
+            },
+            stone:{
+                color: new THREE.Color(0xffffff),
+                texture: 'stone.png'
+            },
+            treeBark:{
+                color: new THREE.Color(0xffffff),
+                texture: 'treeBark.png'
+            },
+            treeLeaves:{
+                color: new THREE.Color(0xffffff),
+                texture: 'treeLeaves.png'
+            },
 
+        }
+    }
 })();
