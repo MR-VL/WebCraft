347356a6729771057387bcc038248381d77448eb
MR-VL <cosmmo6@gmail.com>
2025-01-14 13:35:36 -0600

    init + typo

--- src/sparseVoxelCellManager.js
+++ src/sparseVoxelCellManager.js
@@ -4,3 +4,14 @@ import {utils} from "./Utils.js";
 import {voxelBuilderThreaded} from "./voxelBuilderThreaded.js";
 import {voxelShader} from "./voxelShader.js";
 import {textures} from "./textures.js";
+import {textureDefs} from "./textureDefs.js";
+import {defs} from "./defs.js";
+import {GameDefs} from "./Game-defs.js";
+
+export const sparseVoxelCellManager = (() =>{
+
+
+    return{
+        SparseVoxelCellManager: SparseVoxelCellManager
+    }
+})();
