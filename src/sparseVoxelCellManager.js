3e41f8a959ef74dd5f4b67a8498b6441febbc297
MR-VL <cosmmo6@gmail.com>
2025-01-14 13:39:45 -0600

    init + constructor

--- src/sparseVoxelCellManager.js
+++ src/sparseVoxelCellManager.js
@@ -10,6 +10,24 @@ import {GameDefs} from "./Game-defs.js";
 
 export const sparseVoxelCellManager = (() =>{
 
+    class SparseVoxelCellManager extends entity.Component{
+        static className = "SparseVoxelCellManager";
+        get Name(){
+            return "SparseVoxelCellManager".className;
+        }
+
+        constructor(params) {
+            super();
+            this.blocks = {};
+            this.cellDimensions = new THREE.Vector3(params.cellSize, params.cellSize, params.cellSize);
+            this.visibleDimensions = [params.worldSize, params.worldSize];
+            this.dirtyBlocks = {};
+            this.ids = 0;
+            this.totalTime = 0.0;
+        }
+
+
+    }
 
     return{
         SparseVoxelCellManager: SparseVoxelCellManager
