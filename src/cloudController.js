fafa0d6a579e75c13d70a1694b4a7e1d8539ba9c
MR-VL <cosmmo6@gmail.com>
2025-01-17 15:57:19 -0600

    init cloud entity

--- src/cloudController.js
+++ src/cloudController.js
@@ -6,6 +6,59 @@ import{voxelShader} from "./voxelShader.js";
 
 export const cloudController = (function(){
     class CloudController extends entity.Component{
+        static className = 'CloudController';
+
+        get Name(){
+            return CloudController.className;
+        }
+
+        constructor() {
+            super();
+            this.clouds = [];
+        }
+
+        InitEntity(){
+            const threejs = this.FindEntity('renderer').GetComponent('ThreeJSController');
+            const geometry = new THREE.BoxGeometry(1, 1, 1);
+            this.group = new THREE.Group();
+
+            for(let i =0; i<20; ++i){
+                const width = math.randomInt(5, 10) *20;
+                const length = math.randomInt(5, 10) * 20;
+
+                const x = math.randomInt(-150, 150) * 10;
+                const y = math.randomInt(0, 0) * 25;
+                const z = math.randomInt(-150, 150) * 10;
+
+                const material = new THREE.ShaderMaterial({
+                    uniforms: {
+                        cloudMin:{
+                            value: null
+                        },
+                        cloudMax:{
+                            value: null
+                        }
+                    },
+
+                    vertexShader: voxelShader.CLOUD.vectorShader,
+                    fragmentShader: voxelShader.CLOUD.precisionShader,
+                    side: THREE.FrontSide,
+                    transparent: true
+                });
+
+                const box = new THREE.Mesh(geometry, material);
+                box.position.set(x, y, z);
+                box.scale.set(width, 50, length);
+                this.group.add(box);
+                this.clouds.push(box);
+            }
+
+            this.group.visible = !GameDefs.skipClouds;
+            threejs.scene.add(this.group);
+            this.CreateSun();
+        }
+
+
 
     }
 
