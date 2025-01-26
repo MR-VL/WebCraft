40c293b01d4c5c11eb2bd029063a01b90aa878f0
MR-VL <cosmmo6@gmail.com>
2025-01-26 17:25:16 -0600

    ui controller

--- src/uiController.js
+++ src/uiController.js
@@ -4,7 +4,35 @@ import {GameDefs} from "./Game-Defs.js";
 
 export const uiController = (() => {
     class UIController extends entity.Component{
+        static className = "UIController";
 
+        get Name(){
+            return UIController.className;
+        }
+
+        constructor() {
+            super();
+        }
+
+        InitEntity(){
+            this.iconbar = document.getElementById('icon-bar');
+            this.icons = [];
+
+            const blockTypes = [
+                'dirt', 'stone', 'sand', 'grass', 'snow', 'moon', 'tree_bark', 'tree_leaves'
+            ];
+
+            for(let block of blockTypes){
+                const ent = document.createElement('DIV');
+                let textureName = textureDefs.DEFS[block].texture;
+                if(textureName instanceof Array){
+                    textureName = textureName[2];
+                }
+
+                ent.className = 'icon-bar-item';
+
+            }
+        }
     }
 
     return{
