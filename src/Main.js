e3a86e2698b8db8ab4be3e8a39513bae558969f5
MR-VL <cosmmo6@gmail.com>
2025-01-26 18:08:30 -0600

    load Controllers

--- src/Main.js
+++ src/Main.js
@@ -23,7 +23,39 @@ class WebCraft{
     }
 
     LoadControllers() {
+        const threejs = new entity.Entity();
+        threejs.AddComponent(new threeJSComponents.ThreeJSController());
+        this.entityManager.Add(threejs, 'renderer');
 
+        this.renderer = threejs.GetComponent('THREEJSController');
+        this.scene = threejs.GetComponent('THREEJSController').scene;
+        this.camera = threejs.GetComponent('THREEJSController').camera;
+        this.threejs = threejs.GetComponent('THREEJSController').threejs;
+
+        const voxelManager = new entity.Entity();
+        voxelManager.AddComponent(new sparseVoxelCellManager.SparseVoxelCellManager({
+            cellSize: GameDefs.worldBlockSize,
+            worldSize: GameDefs.worldSize
+        }));
+
+        this.entityManager.Add(voxelManager, 'voxels');
+
+        const clouds = new entity.Entity();
+        clouds.AddComponent(new cloudController.CloudController());
+        this.entityManager.Add(clouds);
+
+        const player = new entity.Entity();
+        player.AddComponent(new playerController.PlayerController());
+        player.AddComponent(new voxelTools.VoxelToolsInsert());
+        player.AddComponent(new voxelTools.VoxelToolsDelete());
+        player.SetPosition(new THREE.Vector3(...GameDefs.playerPOS));
+        player.SetQuaternion(new THREE.Quaternion(...GameDefs.playerROT));
+
+        this.entityManager.Add(player, 'player');
+
+        const ui = new entity.Entity();
+        ui.AddComponent(new uiController.UIController());
+        this.entityManager.Add(ui, 'ui');
     }
 
     RAF(){
